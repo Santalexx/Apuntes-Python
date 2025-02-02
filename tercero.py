62678974f8d869ee8821690c5fbb61038bf1aff2
Alex Fuentes <s4nt4lexx@gmail.com>
2025-02-02 15:57:29 -0500

    Ejercicio con tuplas

--- tercero.py
+++ tercero.py
@@ -33,3 +33,13 @@ frutas.append('manzana')
 frutas.append('pera')
 frutas.append('uva')
 print(frutas)
+
+# Hacer una tupla y agregar varios elementos a cada uno
+
+frutas = ('fresas', 'papaya', 'naranja')
+temporal = list(frutas)
+temporal.append('manzana')
+temporal.append('pera')
+temporal.append('uva')
+frutas = tuple(temporal)
+print(frutas)
